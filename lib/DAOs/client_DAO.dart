209d4472ba386d5af236028d09a0a7e4e46956b7
Robin Hermet <robin.hermet48@gmail.com>
2023-04-24 16:22:09 +0200

    Some modifications

---
+++ lib/DAOs/client_DAO.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:image_upload/utils/constants.dart';
+
+class ApiService {
+
+  static Future<void> createClient({
+    required String full_name,
+    required String pseudo,
+    required String email,
+    required String firebaseId,
+  }) async {
+    final String apiUrl = Constants.baseUrl + Constants.clientEndpoint;
+
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      body: jsonEncode({
+        'full_name': full_name,
+        'pseudo': pseudo,
+        'email': email,
+        'firebaseId': firebaseId,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      print('Client created successfully');
+    } else if (response.statusCode == 400) {
+      throw Exception(response.body); // Récupérer le message d'erreur
+    } else {
+      print('Failed to create client');
+    }
+  }
+
+  static Future<Map<String, dynamic>> checkEmailPseudoUnique({
+    required String email,
+    required String pseudo,
+  }) async {
+    final String apiUrl = Constants.baseUrl + Constants.clientEndpoint + Constants.checkEndpoint;
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({'email': email, 'pseudo': pseudo}),
+    );
+
+    if (response.statusCode == 201) {
+      // If the server returns a successful response
+      Map<String, dynamic> responseBody = json.decode(response.body);
+      return responseBody;
+    } else {
+      // If the server returns an error response
+      throw Exception('Failed to check email and pseudo uniqueness');
+    }
+  }
+
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+
+  //API related
+  static String baseUrl = 'https://us-central1-econet-api.cloudfunctions.net/api';
+  static String clientEndpoint = '/client';
+  static String ecospotEndpoint = '/ecospot';
+  static String typeEndpoint = '/type';
+  static String checkEndpoint = '/checkValidity';
+}
